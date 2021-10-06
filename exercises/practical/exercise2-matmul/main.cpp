60cf870393044ed5563ca4aa9d105f7a1ffb3258
Aaron McUmber <aaron.mcumber@gmail.com>
2021-10-06 13:34:58 -0700

    start on main for matmul

--- exercises/practical/exercise2-matmul/main.cpp
+++ exercises/practical/exercise2-matmul/main.cpp
@@ -8,5 +8,14 @@
 #include "funcs.h"
 
 int main() {
+    vector < vector<int> > mat1 {
+        vector<int> {1, 2, 3},
+        vector<int> {4, 5, 6},
+    }
+    vector < vector<int> > mat2 {
+        vector<int> {9, 8, 7, 6},
+        vector<int> {7, 6, 5, 4},
+        vector<int> {5, 4, 3, 2},
+    }
     return 0;
 }
